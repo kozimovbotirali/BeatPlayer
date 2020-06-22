kozimovbotirali/BeatPlayer

commit 5f21627dfe9f4f2f8d3c924cadc9ad761ba0f7f8

New Notification Behaviour, UI Changes, Code Refactoring
New Notification Behaviour:
- When app starts the notification doesn't appear.
- When current song is paused and click on play button the notification will appear.
- If pause the song by clicking on notification's pause button the notification won't disappear.
- If pause the song by clicking on UI's pause button the notification will disappear.

UI Changes:
- In now playing view the cover image size decreases to 90% when song is paused.
- In now playing view the cover image size increases to 100% when song is playing.
- Changes added to landscape and portrait modes.

Code Refactoring:
- Some unused library imports were removed.
- Some unused variable assignations.
- Reformat code.

Signed-off-by: cramosl3 <cramosl3@miumg.edu.gt>

app/src/main/java/com/crrl/beatplayer/playback/players/BeatPlayer.kt
@@ -15,6 +15,7 @@ package com.crrl.beatplayer.playback.players
 
 import android.app.Application
 import android.app.PendingIntent
+import android.os.Bundle
 import android.os.Handler
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.MediaMetadataCompat.*
@@ -32,6 +33,7 @@ import com.crrl.beatplayer.extensions.toQueueList
 import com.crrl.beatplayer.models.Song
 import com.crrl.beatplayer.playback.AudioFocusHelper
 import com.crrl.beatplayer.repository.SongsRepository
+import com.crrl.beatplayer.utils.BeatConstants.BY_UI_KEY
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_ALL
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_MODE
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_ONE
@@ -44,11 +46,11 @@ import com.crrl.beatplayer.utils.SettingsUtility
 
 interface BeatPlayer {
     fun getSession(): MediaSessionCompat
-    fun playSong()
+    fun playSong(extras: Bundle = bundleOf(BY_UI_KEY to true))
     fun playSong(id: Long)
     fun playSong(song: Song)
     fun seekTo(position: Int)
-    fun pause()
+    fun pause(extras: Bundle = bundleOf(BY_UI_KEY to true))
     fun nextSong()
     fun repeatSong()
     fun repeatQueue()
@@ -81,7 +83,7 @@ class BeatPlayerImplementation(
 
     private var isInitialized: Boolean = false
 
-    private var isPlayingCallback: OnIsPlaying = {}
+    private var isPlayingCallback: OnIsPlaying = { _, _ -> }
     private var preparedCallback: OnPrepared<BeatPlayer> = {}
     private var errorCallback: OnError<BeatPlayer> = {}
     private var completionCallback: OnCompletion<BeatPlayer> = {}
@@ -129,17 +131,18 @@ class BeatPlayerImplementation(
                 REPEAT_MODE_ALL -> {
                     controller.transportControls.sendCustomAction(REPEAT_ALL, null)
                 }
-                else -> if(queueUtils.nextSongId == null) goToStart() else nextSong()
+                else -> if (queueUtils.nextSongId == null) goToStart() else nextSong()
             }
         }
     }
 
     override fun getSession(): MediaSessionCompat = mediaSession
 
-    override fun playSong() {
+    override fun playSong(extras: Bundle) {
         if (isInitialized) {
             updatePlaybackState {
                 setState(STATE_PLAYING, mediaSession.position(), 1F)
+                setExtras(extras)
             }
             musicPlayer.play()
             return
@@ -169,6 +172,7 @@ class BeatPlayerImplementation(
             isInitialized = false
             updatePlaybackState {
                 setState(STATE_PAUSED, 0, 1F)
+                setExtras(bundleOf(BY_UI_KEY to false))
             }
         }
         setMetaData(song)
@@ -194,11 +198,12 @@ class BeatPlayerImplementation(
         }
     }
 
-    override fun pause() {
+    override fun pause(extras: Bundle) {
         if (musicPlayer.isPlaying() && isInitialized) {
             musicPlayer.pause()
             updatePlaybackState {
                 setState(STATE_PAUSED, mediaSession.position(), 1F)
+                setExtras(extras)
             }
         }
     }
@@ -291,9 +296,9 @@ class BeatPlayerImplementation(
             mediaSession.setShuffleMode(bundle.getInt(SHUFFLE_MODE))
         }
         if (state.isPlaying) {
-            isPlayingCallback(this, true)
+            isPlayingCallback(this, true, state.extras?.getBoolean(BY_UI_KEY)!!)
         } else {
-            isPlayingCallback(this, false)
+            isPlayingCallback(this, false, state.extras?.getBoolean(BY_UI_KEY)!!)
         }
     }
 
@@ -332,7 +337,7 @@ class BeatPlayerImplementation(
         }
     }
 
-    private fun goToStart(){
+    private fun goToStart() {
         isInitialized = false
 
         updatePlaybackState {

app/src/main/java/com/crrl/beatplayer/playback/players/MediaSessionCallback.kt
@@ -16,12 +16,16 @@ package com.crrl.beatplayer.playback.players
 import android.media.AudioManager
 import android.os.Bundle
 import android.support.v4.media.session.MediaSessionCompat
-import android.support.v4.media.session.PlaybackStateCompat
 import android.support.v4.media.session.PlaybackStateCompat.*
+import androidx.core.os.bundleOf
+import com.crrl.beatplayer.extensions.isPlaying
 import com.crrl.beatplayer.extensions.toIdList
 import com.crrl.beatplayer.extensions.toMediaId
 import com.crrl.beatplayer.playback.AudioFocusHelper
 import com.crrl.beatplayer.repository.SongsRepository
+import com.crrl.beatplayer.utils.BeatConstants.BY_UI_KEY
+import com.crrl.beatplayer.utils.BeatConstants.PAUSE_ACTION
+import com.crrl.beatplayer.utils.BeatConstants.PLAY_ACTION
 import com.crrl.beatplayer.utils.BeatConstants.PLAY_ALL_SHUFFLED
 import com.crrl.beatplayer.utils.BeatConstants.QUEUE_INFO_KEY
 import com.crrl.beatplayer.utils.BeatConstants.QUEUE_LIST_KEY
@@ -30,7 +34,6 @@ import com.crrl.beatplayer.utils.BeatConstants.REMOVE_SONG
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_ALL
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_MODE
 import com.crrl.beatplayer.utils.BeatConstants.REPEAT_ONE
-import com.crrl.beatplayer.utils.BeatConstants.RESTORE_MEDIA_SESSION
 import com.crrl.beatplayer.utils.BeatConstants.SEEK_TO
 import com.crrl.beatplayer.utils.BeatConstants.SET_MEDIA_STATE
 import com.crrl.beatplayer.utils.BeatConstants.SHUFFLE_MODE
@@ -49,8 +52,7 @@ class MediaSessionCallback(
     init {
         audioFocusHelper.onAudioFocusGain {
             Timber.d("GAIN")
-            val isPlaying = musicPlayer.getSession().controller.playbackState.state == STATE_PLAYING
-            if (isAudioFocusGranted && !isPlaying) {
+            if (isAudioFocusGranted && !musicPlayer.getSession().isPlaying()) {
                 musicPlayer.playSong()
             } else audioFocusHelper.setVolume(AudioManager.ADJUST_RAISE)
             isAudioFocusGranted = false
@@ -64,8 +66,7 @@ class MediaSessionCallback(
 
         audioFocusHelper.onAudioFocusLossTransient {
             Timber.d("TRANSIENT")
-            val isPlaying = musicPlayer.getSession().controller.playbackState.state == STATE_PLAYING
-            if (isPlaying) {
+            if (musicPlayer.getSession().isPlaying()) {
                 isAudioFocusGranted = true
                 musicPlayer.pause()
             }
@@ -84,8 +85,7 @@ class MediaSessionCallback(
 
     override fun onPlay() {
         Timber.d("onPlay()")
-        if (audioFocusHelper.requestPlayback())
-            musicPlayer.playSong()
+        playOnFocus()
     }
 
     override fun onPlayFromSearch(query: String?, extras: Bundle?) {
@@ -152,7 +152,7 @@ class MediaSessionCallback(
         super.onSetShuffleMode(shuffleMode)
         val bundle = mediaSession.controller.playbackState.extras ?: Bundle()
         musicPlayer.setPlaybackState(
-            PlaybackStateCompat.Builder(mediaSession.controller.playbackState)
+            Builder(mediaSession.controller.playbackState)
                 .setExtras(bundle.apply {
                     putInt(SHUFFLE_MODE, shuffleMode)
                 }).build()
@@ -164,8 +164,9 @@ class MediaSessionCallback(
             SET_MEDIA_STATE -> setSavedMediaSessionState()
             REPEAT_ONE -> musicPlayer.repeatSong()
             REPEAT_ALL -> musicPlayer.repeatQueue()
-            RESTORE_MEDIA_SESSION -> restoreMediaSession()
             REMOVE_SONG -> musicPlayer.removeFromQueue(extras?.getLong(SONG_KEY)!!)
+            PLAY_ACTION -> musicPlayer.pause(extras ?: bundleOf(BY_UI_KEY to true))
+            PAUSE_ACTION -> playOnFocus(extras ?: bundleOf(BY_UI_KEY to true))
 
             UPDATE_QUEUE -> {
                 extras ?: return
@@ -209,5 +210,10 @@ class MediaSessionCallback(
             mediaSession.controller.queue.toIdList(),
             mediaSession.controller.queueTitle.toString()
         )
+    }
+
+    private fun playOnFocus(extras: Bundle = bundleOf(BY_UI_KEY to true)){
+        if (audioFocusHelper.requestPlayback())
+            musicPlayer.playSong(extras)
     }
 }

app/src/main/java/com/crrl/beatplayer/MainModel.kt
@@ -21,7 +21,7 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val mainModel = module {
-    factory {
+    single {
         val component = ComponentName(get(), BeatPlayerService::class.java)
         PlaybackConnectionImplementation(get(), component)
     } bind PlaybackConnection::class

app/src/main/java/com/crrl/beatplayer/alias/TypeAlias.kt
@@ -20,7 +20,7 @@ typealias OnPrepared<T> = T.() -> Unit
 typealias OnError<T> = T.(error: Throwable) -> Unit
 typealias OnCompletion<T> = T.() -> Unit
 typealias OnQueueEndWithNoneRepeat<T> = T.() -> Unit
-typealias OnIsPlaying = BeatPlayer.(playing: Boolean) -> Unit
+typealias OnIsPlaying = BeatPlayer.(playing: Boolean, byUi: Boolean) -> Unit
 typealias LiveDataFilter<T> = (T) -> Boolean
 typealias OnAudioFocusGain = AudioFocusHelper.() -> Unit
 typealias OnAudioFocusLoss = AudioFocusHelper.() -> Unit

app/src/main/java/com/crrl/beatplayer/extensions/ViewExtensions.kt
@@ -130,7 +130,7 @@ fun View?.scaleDown() {
     val view = this ?: return
     please(250, AccelerateDecelerateInterpolator()) {
         animate(view) {
-            scale(0.6f, 0.6f)
+            scale(0.9f, 0.9f)
         }
     }.start()
 }

app/src/main/java/com/crrl/beatplayer/models/MediaItemData.kt
@@ -26,22 +26,21 @@ class MediaItemData(
 
 ) {
     companion object {
-        fun pullMediaMetadata(metaData: MediaMetadataCompat?): MediaItemData {
-            metaData?.let {
-                val mediaId = metaData.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)
-                return MediaItemData(
-                    id = if (mediaId == "") 0 else mediaId.toLong(),
-                    title = metaData.getString(MediaMetadataCompat.METADATA_KEY_TITLE) ?: "",
-                    album = metaData.getString(MediaMetadataCompat.METADATA_KEY_ALBUM) ?: "",
-                    artist = metaData.getString(MediaMetadataCompat.METADATA_KEY_ARTIST) ?: "",
-                    duration = metaData.getLong(MediaMetadataCompat.METADATA_KEY_DURATION).toInt(),
-                    albumId = metaData.getString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI)
-                        ?.toLong() ?: 0,
-                    description = metaData.getString(MediaMetadataCompat.METADATA_KEY_DISPLAY_DESCRIPTION)
-                        ?: ""
-                )
-            }
-            return MediaItemData()
+        fun pullMediaMetadata(metaData: MediaMetadataCompat?): MediaItemData? {
+            metaData ?: return null
+            val mediaId = metaData.getString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID)
+            val data = MediaItemData(
+                id = if (mediaId == "") -1 else mediaId.toLong(),
+                title = metaData.getString(MediaMetadataCompat.METADATA_KEY_TITLE) ?: "",
+                album = metaData.getString(MediaMetadataCompat.METADATA_KEY_ALBUM) ?: "",
+                artist = metaData.getString(MediaMetadataCompat.METADATA_KEY_ARTIST) ?: "",
+                duration = metaData.getLong(MediaMetadataCompat.METADATA_KEY_DURATION).toInt(),
+                albumId = metaData.getString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI)
+                    ?.toLong() ?: 0,
+                description = metaData.getString(MediaMetadataCompat.METADATA_KEY_DISPLAY_DESCRIPTION)
+                    ?: ""
+            )
+            return if (data.id != -1L) data else null
         }
     }
 }

app/src/main/java/com/crrl/beatplayer/notifications/Notifications.kt
@@ -41,7 +41,6 @@ import kotlinx.coroutines.launch
 import androidx.media.app.NotificationCompat as NotificationMediaCompat
 
 interface Notifications {
-
     fun updateNotification(mediaSession: MediaSessionCompat)
     fun buildNotification(mediaSession: MediaSessionCompat): Notification
     fun clearNotifications()
@@ -53,6 +52,7 @@ class NotificationsImplementation(
 ) : Notifications {
 
     override fun updateNotification(mediaSession: MediaSessionCompat) {
+        if (!BeatPlayerService.IS_RUNNING) return
         GlobalScope.launch {
             notificationManager.notify(NOTIFICATION_ID, buildNotification(mediaSession))
         }

app/src/main/java/com/crrl/beatplayer/playback/BeatPlayerService.kt
@@ -18,6 +18,7 @@ import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
 import android.support.v4.media.session.PlaybackStateCompat.STATE_NONE
+import androidx.core.os.bundleOf
 import androidx.media.MediaBrowserServiceCompat
 import androidx.media.session.MediaButtonReceiver
 import com.crrl.beatplayer.R
@@ -31,8 +32,11 @@ import com.crrl.beatplayer.playback.players.BeatPlayer
 import com.crrl.beatplayer.playback.receivers.BecomingNoisyReceiver
 import com.crrl.beatplayer.repository.*
 import com.crrl.beatplayer.utils.BeatConstants
+import com.crrl.beatplayer.utils.BeatConstants.BY_UI_KEY
 import com.crrl.beatplayer.utils.BeatConstants.NEXT
 import com.crrl.beatplayer.utils.BeatConstants.NOTIFICATION_ID
+import com.crrl.beatplayer.utils.BeatConstants.PAUSE_ACTION
+import com.crrl.beatplayer.utils.BeatConstants.PLAY_ACTION
 import com.crrl.beatplayer.utils.BeatConstants.PLAY_PAUSE
 import com.crrl.beatplayer.utils.BeatConstants.PREVIOUS
 import com.crrl.beatplayer.utils.SettingsUtility
@@ -45,6 +49,10 @@ import org.koin.core.KoinComponent
 import org.koin.core.inject
 
 class BeatPlayerService : MediaBrowserServiceCompat(), KoinComponent {
+
+    companion object {
+        var IS_RUNNING = false
+    }
 
     private lateinit var becomingNoisyReceiver: BecomingNoisyReceiver
 
@@ -65,18 +73,17 @@ class BeatPlayerService : MediaBrowserServiceCompat(), KoinComponent {
         sessionToken = beatPlayer.getSession().sessionToken
         becomingNoisyReceiver = BecomingNoisyReceiver(this, sessionToken!!)
 
-        beatPlayer.onPlayingState { isPlaying ->
+        beatPlayer.onPlayingState { isPlaying, byUi ->
             if (isPlaying) {
                 startForeground(NOTIFICATION_ID, notifications.buildNotification(getSession()))
                 becomingNoisyReceiver.register()
             } else {
                 becomingNoisyReceiver.unregister()
-                stopForeground(false)
                 saveCurrentData()
+                stopForeground(byUi)
+                if (!byUi) notifications.updateNotification(getSession())
             }
-
-            if (getSession().controller.playbackState.state != 0)
-                notifications.updateNotification(getSession())
+            IS_RUNNING = isPlaying
         }
 
         beatPlayer.onCompletion {
@@ -100,8 +107,14 @@ class BeatPlayerService : MediaBrowserServiceCompat(), KoinComponent {
             PLAY_PAUSE -> {
                 controller.playbackState?.let { playbackState ->
                     when {
-                        playbackState.isPlaying -> controller.transportControls.pause()
-                        playbackState.isPlayEnabled -> controller.transportControls.play()
+                        playbackState.isPlaying -> controller.transportControls.sendCustomAction(
+                            PLAY_ACTION,
+                            bundleOf(BY_UI_KEY to false)
+                        )
+                        playbackState.isPlayEnabled -> controller.transportControls.sendCustomAction(
+                            PAUSE_ACTION,
+                            bundleOf(BY_UI_KEY to false)
+                        )
                     }
                 }
             }
@@ -141,13 +154,6 @@ class BeatPlayerService : MediaBrowserServiceCompat(), KoinComponent {
             CALLER_OTHER
         }
         return BrowserRoot(MediaId("-1", null, caller).toString(), null)
-    }
-
-    override fun onDestroy() {
-        saveCurrentData()
-        beatPlayer.release()
-        notifications.clearNotifications()
-        super.onDestroy()
     }
 
     private fun saveCurrentData() {

commit b2d6ca9d3d06d91486d38cf9ad4889a7433946d4

Fix some issues caused by 5f21627 and Fix request audio focus
The commit 5f21627 caused that shuffle mode and repeat mode change to MODE_NONE every time the playback state changes.

Request audio focus: when opening the application (play paused) and then pressing the play all or randomly play all button, the audio focus was not requested, nor when the current Playback view was opened and then it was pressed the next or previous button.

app/src/main/java/com/crrl/beatplayer/extensions/GeneralExtensions.kt
@@ -18,8 +18,6 @@ import android.os.Bundle
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
 import com.crrl.beatplayer.utils.BeatConstants.QUEUE_INFO_KEY
 import com.crrl.beatplayer.utils.BeatConstants.SEEK_TO_POS
 import com.crrl.beatplayer.utils.BeatConstants.SONG_LIST_NAME
@@ -36,6 +34,12 @@ fun MediaSessionCompat.position(): Long {
 fun MediaSessionCompat.isPlaying(): Boolean {
     return controller.playbackState.state == PlaybackStateCompat.STATE_PLAYING
 }
+
+inline val MediaSessionCompat.repeatMode
+    get() = controller.repeatMode
+
+inline val MediaSessionCompat.shuffleMode
+    get() = controller.shuffleMode
 
 inline val PlaybackStateCompat.isPrepared
     get() = (state == PlaybackStateCompat.STATE_BUFFERING) ||
@@ -75,10 +79,9 @@ fun getExtraBundle(queue: LongArray, title: String, seekTo: Int?): Bundle? {
     return bundle
 }
 
-fun <X, Y> LiveData<X>.map(mapper: (X) -> Y) =
-    Transformations.map(this, mapper)
-
 @Suppress("UNCHECKED_CAST")
 fun <T> Context.systemService(name: String): T {
     return getSystemService(name) as T
 }
+
+operator fun Bundle.plus(other: Bundle) = this.apply { putAll(other) }

app/src/main/java/com/crrl/beatplayer/playback/players/BeatPlayer.kt
@@ -26,10 +26,7 @@ import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 import com.crrl.beatplayer.R
 import com.crrl.beatplayer.alias.*
-import com.crrl.beatplayer.extensions.isPlaying
-import com.crrl.beatplayer.extensions.position
-import com.crrl.beatplayer.extensions.toQueueInfo
-import com.crrl.beatplayer.extensions.toQueueList
+import com.crrl.beatplayer.extensions.*
 import com.crrl.beatplayer.models.Song
 import com.crrl.beatplayer.playback.AudioFocusHelper
 import com.crrl.beatplayer.repository.SongsRepository
@@ -78,7 +75,7 @@ class BeatPlayerImplementation(
     private val songsRepository: SongsRepository,
     private val settingsUtility: SettingsUtility,
     private val queueUtils: QueueUtils,
-    audioFocusHelper: AudioFocusHelper
+    private val audioFocusHelper: AudioFocusHelper
 ) : BeatPlayer {
 
     private var isInitialized: Boolean = false
@@ -99,7 +96,7 @@ class BeatPlayerImplementation(
                 MediaSessionCallback(
                     this,
                     this@BeatPlayerImplementation,
-					audioFocusHelper,
+                    audioFocusHelper,
                     songsRepository
                 )
             )
@@ -142,7 +139,12 @@ class BeatPlayerImplementation(
         if (isInitialized) {
             updatePlaybackState {
                 setState(STATE_PLAYING, mediaSession.position(), 1F)
-                setExtras(extras)
+                setExtras(
+                    extras + bundleOf(
+                        REPEAT_MODE to getSession().repeatMode,
+                        SHUFFLE_MODE to getSession().shuffleMode
+                    )
+                )
             }
             musicPlayer.play()
             return
@@ -162,8 +164,10 @@ class BeatPlayerImplementation(
     }
 
     override fun playSong(id: Long) {
-        val song = songsRepository.getSongForId(id)
-        playSong(song)
+        if (audioFocusHelper.requestPlayback()){
+            val song = songsRepository.getSongForId(id)
+            playSong(song)
+        }
     }
 
     override fun playSong(song: Song) {
@@ -172,7 +176,13 @@ class BeatPlayerImplementation(
             isInitialized = false
             updatePlaybackState {
                 setState(STATE_PAUSED, 0, 1F)
-                setExtras(bundleOf(BY_UI_KEY to false))
+                setExtras(
+                    bundleOf(
+                        BY_UI_KEY to false,
+                        REPEAT_MODE to getSession().repeatMode,
+                        SHUFFLE_MODE to getSession().shuffleMode
+                    )
+                )
             }
         }
         setMetaData(song)
@@ -203,7 +213,12 @@ class BeatPlayerImplementation(
             musicPlayer.pause()
             updatePlaybackState {
                 setState(STATE_PAUSED, mediaSession.position(), 1F)
-                setExtras(extras)
+                setExtras(
+                    extras + bundleOf(
+                        REPEAT_MODE to getSession().repeatMode,
+                        SHUFFLE_MODE to getSession().shuffleMode
+                    )
+                )
             }
         }
     }

app/src/main/java/com/crrl/beatplayer/playback/players/MediaSessionCallback.kt
@@ -114,8 +114,7 @@ class MediaSessionCallback(
             musicPlayer.seekTo(seekTo)
         }
 
-        if (audioFocusHelper.requestPlayback())
-            musicPlayer.playSong(songId)
+        musicPlayer.playSong(songId)
     }
 
     override fun onSeekTo(pos: Long) {
@@ -212,7 +211,7 @@ class MediaSessionCallback(
             mediaSession.controller.queueTitle.toString()
         )
     }
-	
+
 	private fun playOnFocus(extras: Bundle = bundleOf(BY_UI_KEY to true)) {
         if (audioFocusHelper.requestPlayback())
             musicPlayer.playSong(extras)